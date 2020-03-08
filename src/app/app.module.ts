89ec63e989cca5e58d3c1c7674b8b9d65ae9e072
Myles Keeffe <11828103+myleskeeffe@users.noreply.github.com>
2020-03-08 14:54:20 +0800

    Basic angular app setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MsalModule } from '@azure/msal-angular';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule.forRoot({
+      clientID: "ClientTenantID",
+      authority: "https://login.microsoftonline.com/common",
+      redirectUri: "http://localhost:4200/",
+      consentScopes: [ "user.read", "ClientID/access_as_user"],
+      popUp: false,
+      correlationId: '1234',
+      level: 1,
+      piiLoggingEnabled: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { map } from "rxjs/operators";
+
+
+import { fromEventPattern } from 'rxjs';
+var IDToken = "";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(public http: HttpClient, headers: HttpHeaders) {
+
+  }
+
+  getTodos(){
+
+    return new Promise((resolve, reject) => {
+
+      let headers = new Headers();
+      headers.append('Authorization', "fixme");
+
+      this.http.get('https://YOUR_HEROKU_APP.herokuapp.com/api/todos', {headers: { Authorization: IDToken}})
+        .subscribe(data => {
+          resolve(data);
+        }, (err) => {
+          reject(err);
+        });
+    });
+
+  }
+
+  createTodo(todo){
+
+    return new Promise((resolve, reject) => {
+
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      headers.append('Authorization', "fixme");
+
+      this.http.post('https://YOUR_HEROKU_APP.herokuapp.com/api/todos', JSON.stringify(todo), {headers: { Authorization: IDToken}})
+        .subscribe(res => {
+          resolve(res);
+        }, (err) => {
+          reject(err);
+        });
+
+    });
+
+  }
+
+  deleteTodo(id){
+
+    return new Promise((resolve, reject) => {
+
+        let headers = new Headers();
+        headers.append('Authorization', "fixme");
+
+        this.http.delete('https://YOUR_HEROKU_APP.herokuapp.com/api/todos/' + id, {headers: { Authorization: IDToken}}).subscribe((res) => {
+            resolve(res);
+        }, (err) => {
+            reject(err);
+        });    
+
+    });
+
+  }
+
 }
+
